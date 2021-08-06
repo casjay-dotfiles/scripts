1a9afd6593b5d87a5b26e24fa91fb31e45fff00a
Jason <jason@casjay.net>
2021-08-05 20:17:52 -0400

    🗃 Modified: bin/am_i_online bin/append_path bin/ask_for_input bin/ask_for_password bin/ask_yes_no_question bin/calc bin/cmd_exists bin/wikipedia functions/simple.bash  🗃

--- functions/simple.bash
+++ functions/simple.bash
@@ -804,7 +804,10 @@ __git_update() {
 #git_commit "dir"
 __git_commit() {
   [ -n "$1" ] && local dir="$1" && shift 1 || local dir="${INSTDIR:-.}"
-  if type -P gitcommit &/dev/null; then
+  if
+    type -P gitcommit &
+    /dev/null
+  then
     if [ -d "$2" ]; then shift 1; fi
     local mess="${*}"
     gitcommit "$dir" "$mess"
@@ -1074,6 +1077,48 @@ __if_os_id() {
   # [ -z $distro_version ] || distro_version="Unknown"
   # [ -n "$codename" ] && distro_codename="$codename" || distro_codename="N/A"
   # echo $id_like $distroname $distroversion $distro_codename
+}
+__execute() {
+  __set_trap() { trap -p "$1" | grep "$2" &>/dev/null || trap '$2' "$1"; }
+  __kill_all_subprocesses() {
+    local i=""
+    for i in $(jobs -p); do
+      kill "$i"
+      wait "$i" &>/dev/null
+    done
+  }
+  __show_spinner() {
+    local -r FRAMES='/-\|'
+    local -r NUMBER_OR_FRAMES=${#FRAMES}
+    local -r CMDS="$2"
+    local -r MSG="$3"
+    local -r PID="$1"
+    local i=0
+    local frameText=""
+    while kill -0 "$PID" &>/dev/null; do
+      frameText="                [${FRAMES:i++%NUMBER_OR_FRAMES:1}] $MSG"
+      printf "%s" "$frameText"
+      sleep 0.2
+      printf "\r"
+    done
+  }
+  local -r CMDS="$1"
+  local -r MSG="${2:-$1} "
+  local -r TMP_FILE="$(mktemp /tmp/XXXXX)"
+  local exitCode=0
+  local cmdsPID=""
+  __set_trap "EXIT" "__kill_all_subprocesses"
+  eval "$CMDS" >/dev/null 2>"$TMP_FILE" &
+  cmdsPID=$!
+  __show_spinner "$cmdsPID" "$CMDS" "$MSG"
+  wait "$cmdsPID" &>/dev/null
+  exitCode=$?
+  printf_execute_result $exitCode "$MSG"
+  if [ $exitCode -ne 0 ]; then
+    printf_execute_error_stream <"$TMP_FILE"
+  fi
+  rm -rf "$TMP_FILE"
+  return $exitCode
 }
 ###################### setup folders - user ######################
 user_installdirs() {
@@ -1554,11 +1599,11 @@ __help() {
   exit 0
 }
 __version() {
-  local name="${1:-$(basename $0)}" # get from os
-  local prog="${APPNAME:-$PROG}"    # get from file
-  local appname="${prog:-$name}"    # figure out wich one
-  filename="$(type -P $appname 2>/dev/null)"    # get filename
-  if [ -f "$filename" ]; then       # check for file
+  local name="${1:-$(basename $0)}"          # get from os
+  local prog="${APPNAME:-$PROG}"             # get from file
+  local appname="${prog:-$name}"             # figure out wich one
+  filename="$(type -P $appname 2>/dev/null)" # get filename
+  if [ -f "$filename" ]; then                # check for file
     printf_newline
     printf_green "Getting info for $appname"
     [ -n "$WHOAMI" ] && printf_yellow "WhoamI        : $WHOAMI"

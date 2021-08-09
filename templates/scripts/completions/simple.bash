38fc1772a38869a0c43b4b1ca02adfbe4ad8ba54
Jason <jason@casjay.net>
2021-08-09 13:43:00 -0400

    ❓ Added: templates/scripts/completions/simple.bash  ❓

---
+++ templates/scripts/completions/simple.bash
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="$(basename "$0")"
+VERSION="GEN_SCRIPT_REPLACE_VERSION"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+SRC_DIR="${BASH_SOURCE%/*}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#set opts
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : GEN_SCRIPT_REPLACE_VERSION
+# @Author        : GEN_SCRIPT_REPLACE_AUTHOR
+# @Contact       : GEN_SCRIPT_REPLACE_EMAIL
+# @License       : GEN_SCRIPT_REPLACE_LICENSE
+# @ReadME        : GEN_SCRIPT_REPLACE_FILENAME --help
+# @Copyright     : GEN_SCRIPT_REPLACE_COPYRIGHT
+# @Created       : GEN_SCRIPT_REPLACE_DATE
+# @File          : GEN_SCRIPT_REPLACE_FILENAME
+# @Description   : GEN_SCRIPT_REPLACE_DESC
+# @TODO          : GEN_SCRIPT_REPLACE_TODO
+# @Other         : GEN_SCRIPT_REPLACE_OTHER
+# @Resource      : GEN_SCRIPT_REPLACE_RES
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+_GEN_SCRIPT_REPLACE_FILENAME() {
+  local CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+  local cur prev words cword
+  local cur="${COMP_WORDS[$COMP_CWORD]}"
+  local LONGOPTS="--help --version --config --options"
+  local SHORTOPTS="-h -v -c"
+  local ARRAY=""
+
+  _init_completion || return
+
+  case ${COMP_WORDS[1]:-$prev} in
+  *)
+    if [[ ${cur} == --* ]]; then
+      COMPREPLY=($(compgen -W '${LONGOPTS}' -- ${cur}))
+    elif [[ ${cur} == -* ]]; then
+      COMPREPLY=($(compgen -W '${SHORTOPTS}' -- ${cur}))
+    else
+      COMPREPLY=($(compgen -W '$ARRAY' -- "$cur"))
+      return 0
+    fi
+    ;;
+  esac
+} &&
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # enable completions
+  complete -F _GEN_SCRIPT_REPLACE_FILENAME am_i_online

412f72ebc166ba314707a2157869daf3974b742c
Jason <jason@casjay.net>
2021-08-20 02:48:00 -0400

    🗃 Modified: bin/mymenu functions/testing.bash  🗃

--- functions/testing.bash
+++ functions/testing.bash
@@ -436,7 +436,7 @@ printf_read_input() {
   local reply="${1:-REPLY}" && shift 1
   local readopts="${1:-}" && shift 1
   printf_color "\t\t$msg " "${PRINTF_COLOR:-$color}"
-  read -r -n $lines ${readopts:-} ${reply:-} || return 1
+  read -e -r -n $lines ${readopts:-} ${reply:-} || return 1
 }
 #printf_read_question "color" "message" "maxLines" "answerVar" "readopts"
 printf_read_question() {

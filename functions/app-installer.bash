0fbf8d9f1f86d43cd25b0090075c6b0680965a43
Jason <jason@casjay.net>
2021-08-20 17:47:56 -0400

    🗃 Modified: bin/pkmgr functions/app-installer.bash  🗃

--- functions/app-installer.bash
+++ functions/app-installer.bash
@@ -898,7 +898,7 @@ install_packages() {
         printf_warning "Attempting to install missing packages as $RUN_USER"
         printf_warning "$MISSING"
         for miss in $MISSING; do
-          if builtin type -P yay 2>/dev/null; then
+          if [ -f "$(builtin type -P yay 2>/dev/null)" ]; then
             execute "pkmgr --enable-aur silent install $miss" "Installing $miss"
           else
             execute "pkmgr silent install $miss" "Installing $miss"

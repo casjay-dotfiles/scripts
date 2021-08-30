c59c2252746b0d75a3001548ceb901d33ce5af81
Jason <jason@casjay.net>
2021-08-30 00:12:52 -0400

    🗃 Modified: functions/app-installer.bash  🗃

--- functions/app-installer.bash
+++ functions/app-installer.bash
@@ -2048,7 +2048,7 @@ __main_installer_info() {
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 run_install_init() {
-  SET_SUDO_PROMPT="$(printf "\t\t\033[1;31m")[sudo]$(printf "\033[1;36m") password for $(printf "\033[1;32m")%p: $(printf "\033[0m")"
+  SET_SUDO_PROMPT="$(printf "\n\t\t\033[1;31m")[sudo]$(printf "\033[1;36m") password for $(printf "\033[1;32m")%p: $(printf "\033[0m")"
   (sudo -vn && sudo -ln) 2>&1 | grep -v 'may not' >/dev/null || sudo -n true &>/dev/null || SUDO_PROMPT="$SET_SUDO_PROMPT" sudo true
   trap '[ -f "$TMPINST" ] && rm_rf $"$TMPINST"' EXIT
   __main_installer_info &>/dev/null

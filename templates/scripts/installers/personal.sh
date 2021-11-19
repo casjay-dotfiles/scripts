827e0898987ece225bed627adf13be0cb753a7ca
Jason <jason@casjay.net>
2021-11-19 09:34:03 -0500

    🗃 Modified: bin/am_i_online bin/anonymize bin/append_path bin/ask_for_input bin/ask_for_password bin/ask_yes_no_question bin/automate bin/backupapp bin/bashtop bin/blog bin/calc bin/calendar bin/changeip bin/cheat.sh bin/check-for-updates bin/check_app bin/clean-system bin/clipboard bin/cloudflare bin/cmd_exists bin/cmdfu bin/command bin/corona-virus bin/covid19 bin/currency bin/decrypt bin/define bin/devenvmgr bin/devhints bin/dfmgr bin/displayselect bin/dmenu-usb bin/dmenurecord bin/dockermgr bin/dockerrun bin/dockerssh bin/dos2unix bin/emojis bin/encrypt bin/eslint-config bin/expandurl bin/explainshell bin/findInstallApp bin/findmypi bin/fontmgr bin/gen-apache bin/gen-caddy bin/gen-dns bin/gen-nginx bin/gen-readme bin/gen-script bin/gitadmin bin/gitcommit bin/gitignore bin/hostname bin/hue bin/iconmgr bin/ifinstalled bin/ifttt bin/images bin/imperial2metric bin/ix.io bin/killproc bin/killzombies bin/latest-releases bin/logout-menu bin/logr bin/lorem bin/mailsync bin/metric2imperial bin/mkpasswd bin/mpd-notify bin/mpdclient bin/multimedia bin/music bin/mybluetooth bin/mybrowser bin/mydate bin/myeditor bin/myemail bin/myfilemanager bin/myip bin/mylocation bin/mymenu bin/myps bin/myshell bin/myssh bin/myterminal bin/mytorrent bin/myvmmanager bin/netstatg bin/netstatl bin/notes bin/notifications bin/notify-daemon bin/passmgr bin/pastebin bin/pb bin/pkmgr bin/post2social bin/pslist bin/pushbullet bin/radioreference bin/random-string bin/randomwallpaper bin/readme-2-post bin/recording bin/reqpkgs bin/roku bin/run_command bin/run_in_term bin/run_server bin/scratchpad bin/screencast bin/screenshot-menu bin/search-menu bin/shortenurl bin/showterm bin/social bin/spellcheck bin/sprunge.us bin/ss_screenkey bin/sshto bin/sudoers bin/systemmgr bin/tasks bin/tell-joke bin/termbin.com bin/thememgr bin/thesaurus bin/timer bin/tldr bin/todo bin/tor-site bin/transfer.sh bin/trash bin/tryforever bin/type bin/update-ip bin/update-motd bin/urbandict bin/urlcheck bin/urldecode bin/urlencode bin/videos bin/wallpapermgr bin/weather bin/wikipedia templates/scripts/bash/case templates/scripts/bash/system templates/scripts/bash/terminal templates/scripts/installers/base-mgr-script.sh templates/scripts/installers/personal.sh  🗃

--- templates/scripts/installers/personal.sh
+++ templates/scripts/installers/personal.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="GEN_SCRIPT_REPLACE_APPNAME"
+APPNAME="$(basename "$0")"
+VERSION="202111171035-git"
 USER="${SUDO_USER:-${USER}}"
 HOME="${USER_HOME:-${HOME}}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Set bash options
-if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
+#set opts
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version       : GEN_SCRIPT_REPLACE_VERSION
@@ -26,79 +26,89 @@ if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OP
 CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
 SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
 SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
-SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/GEN_SCRIPT_REPLACE_DEFAULT_BRANCH/functions}"
-connect_test() { ping -c1 1.1.1.1 &>/dev/null || curl --disable -LSs --connect-timeout 3 --retry 0 --max-time 1 1.1.1.1 2>/dev/null | grep -e "HTTP/[0123456789]" | grep -q "200" -n1 &>/dev/null; }
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/main/functions}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
   . "$PWD/$SCRIPTSFUNCTFILE"
 elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
   . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
-elif connect_test; then
-  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/$SCRIPTSFUNCTFILE" || exit 1
-  . "/tmp/$SCRIPTSFUNCTFILE"
 else
   echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
   exit 1
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# INIT
-clear
-sleep 1
+# user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
+user_installdirs
+show_optvars "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Options
 PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin/core_perl/cpan"
-#Modify and set if using the auth token
-AUTHTOKEN="${GITHUB_ACCESS_TOKEN}"
+# Modify and set if using the auth token
+AUTHTOKEN="${GITHUB_ACCESS_TOKEN:-$MYPERSONAL_GIT_TOKEN}"
 # either http https or git
-GITPROTO="https://"
-#Your git repo
-GITREPO="${MYPERSONALGITREPO:-github.com/dfmgr/personal}"
-#scripts repo
+GIT_PROTO="https://"
+# Your git repo
+GIT_REPO="${MYPERSONAL_GIT_REPO:-github.com/dfmgr/personal}"
+# scripts repo
 SCRIPTSREPO="https://github.com/dfmgr/installer"
-# Git Command - Private Repo
-GITURL="$GITPROTO$AUTHTOKEN:x-oauth-basic@$GITREPO"
-#Public Repo
-#GITURL="$GITPROTO$GITREPO"
 # Default NTP Server
 NTPSERVER="ntp.casjay.net"
-# Default dotfiles dir
 # Set primary dir
 DOTFILES="$HOME/.local/dotfiles/personal"
 # Set the temp directory
 DOTTEMP="/tmp/dotfiles-personal-$USER"
 # Set tmpfile
 TMP_FILE="$(mktemp /tmp/dfm-XXXXXXXXX)"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-printf_green "\t\tInitializing the installer please wait\n"
-sleep 2
-
+# Git Command - Private Repo
+FULL_GIT_URL="$GIT_PROTO$AUTHTOKEN:x-oauth-basic@$GIT_REPO"
+#Public Repo
+#FULL_GIT_URL="$GIT_PROTO$GIT_REPO"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set functions
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/main/functions}"
+SCRIPTSFUNCTDIR="${SCRIPTSAPPFUNCTDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE"
+elif [ -f "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
+else
+  mkdir -p "$HOME/.local/share/CasjaysDev/functions"
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE" || exit 1
+  . "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+clear
+sleep 1
+printf_green "Initializing the installer please wait"
+sleep 2
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 install_all_dfmgr() { dfmgr install --all || return 1; }
 install_basic_dfmgr() { dfmgr install misc bash git vim || return 1; }
 install_server_dfmgr() { dfmgr install bash git tmux vifm vim || return 1; }
 install_desktop_dfmr() { dfmgr install awesome bspwm i3 openbox qtile xfce4 xmonad || return 1; }
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 _pre_inst() {
   if [ -z "$AUTHTOKEN" ] || [ "$AUTHTOKEN" == "YOUR_AUTH_TOKEN" ]; then
-    printf_red "\t\tAUTH Token is not set"
+    printf_red "AUTH Token is not set"
     exit 1
   fi
-  if [ ! -f "$(type -P sudo 2>/dev/null)" ] && [[ $EUID -ne 0 ]]; then
-    printf_red "\t\tSudo is needed, however its not installed installed\n"
+  if [ ! -f "$(which sudo 2>/dev/null)" ] && [[ $EUID -ne 0 ]]; then
+    printf_red "Sudo is needed, however its not installed installed"
     exit 1
   fi
+
   if [ ! -d "$DOTFILES"/.git ]; then rm -Rf "$DOTFILES"; fi
-  rm -Rf "$DOTTEMP" >/dev/null 2>&1
+  rm -Rf "$DOTTEMP" &>/dev/null
+
   if [[ "$OSTYPE" =~ ^linux ]]; then
     if ! cmd_exists systemmgr; then
       if (sudo -vn && sudo -ln) 2>&1 | grep -v 'may not' >/dev/null; then
-        sudo bash -c "$(curl -LSs https://github.com/systemmgr/installer/raw/GEN_SCRIPT_REPLACE_DEFAULT_BRANCH/install.sh)"
-        sudo bash -c "$(curl -LSs https://github.com/systemmgr/installer/raw/GEN_SCRIPT_REPLACE_DEFAULT_BRANCH/install.sh)"
+        sudo bash -c "$(curl -LSs https://github.com/systemmgr/installer/raw/main/install.sh)"
+        sudo bash -c "$(curl -LSs https://github.com/systemmgr/installer/raw/main/install.sh)"
       else
-        printf_red '\t\tplease run sudo bash -c "$(curl -LSs https://github.com/systemmgr/installer/raw/GEN_SCRIPT_REPLACE_DEFAULT_BRANCH/install.sh)\n"'
+        printf_red 'please run sudo bash -c "$(curl -LSs https://github.com/systemmgr/installer/raw/main/install.sh)"'
         exit 1
       fi
     fi
@@ -107,26 +117,22 @@ _pre_inst() {
     sudoers nopass
   fi
 }
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 _git_repo_init() {
   if [ -d "$DOTFILES"/.git ]; then
     git -C "$DOTFILES" pull -f
-    getexitcode "repo update successfull"
+    getexitcode "repo update successfull" "Failed to pull $DOTFILES"
   else
-    git clone "$GITURL" "$DOTFILES"
-    getexitcode "git clone successfull"
+    git clone "$FULL_GIT_URL" "$DOTFILES"
+    getexitcode "git clone successfull" "Failed to clone $FULL_GIT_URL"
   fi
-
-  if [ -d "$DOTFILES" ]; then cp -Rf "$DOTFILES" "$DOTTEMP" >/dev/null 2>&1; fi
+  if [ -d "$DOTFILES" ]; then cp -Rf "$DOTFILES" "$DOTTEMP" &>/dev/null; fi
 }
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 _scripts_init() {
-  for sudoconf in installer ssh; do
-    sudo systemmgr install "$sudoconf"
+  for sudoconf in installer ssl; do
+    sudo systemmgr install $sudoconf
   done
-
   if [[ "$OSTYPE" =~ ^linux ]]; then
     if [ "$1" = "--all" ]; then
       install_all_dfmgr
@@ -141,33 +147,29 @@ _scripts_init() {
     install_basic_dfmgr
   fi
 }
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 _files_init() {
-  find "$HOME/" -xtype l -delete >/dev/null 2>&1
-  mkdir -p "$HOME"/.ssh "$HOME"/.gnupg >/dev/null 2>&1
-  chmod -Rf 755 "$DOTTEMP"/usr/local/bin/* >/dev/null 2>&1
-  chmod -Rf 755 "$DOTTEMP"/root/skel/.local/bin/* >/dev/null 2>&1
-  find "$DOTTEMP"/root -type f -iname "*.bash" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-  find "$DOTTEMP"/root -type f -iname "*.sh" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-  find "$DOTTEMP"/root -type f -iname "*.pl" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-  find "$DOTTEMP"/root -type f -iname "*.cgi" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-  find "$DOTTEMP"/usr -type f -iname "*.bash" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-  find "$DOTTEMP"/usr -type f -iname "*.sh" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-  find "$DOTTEMP"/usr -type f -iname "*.pl" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-  find "$DOTTEMP"/usr -type f -iname "*.cgi" -exec chmod 755 -Rf {} \; >/dev/null 2>&1
-
-  unalias cp 2>/dev/null
-  rsync -ahqk "$DOTTEMP"/root/skel/. "$HOME"/
-
-  export GPG_TTY="$(tty)"
+  GPG_TTY="$(tty)"
+  unalias cp &>/dev/null
+  find "$HOME/" -xtype l -delete &>/dev/null
+  mkdir -p "$HOME"/.ssh "$HOME"/.gnupg &>/dev/null
+  chmod -Rf 755 "$DOTTEMP"/root/skel/.local/bin/* &>/dev/null
+  find "$DOTTEMP"/root -type f -iname "*.bash" -exec chmod 755 -Rf {} \; &>/dev/null
+  find "$DOTTEMP"/root -type f -iname "*.sh" -exec chmod 755 -Rf {} \; &>/dev/null
+  find "$DOTTEMP"/root -type f -iname "*.pl" -exec chmod 755 -Rf {} \; &>/dev/null
+  find "$DOTTEMP"/root -type f -iname "*.cgi" -exec chmod 755 -Rf {} \; &>/dev/null
+  rsync -ahqk "$DOTTEMP"/root/skel/. "$HOME"/ &>/dev/null
   if (sudo -vn && sudo -ln) 2>&1 | grep -v 'may not' >/dev/null; then
-    mv -f "$DOTTEMP"/root/skel "$DOTTEMP"/tmp/skel >/dev/null 2>&1
-    sudo rsync -ahq "$DOTTEMP"/root/* root/ >/dev/null 2>&1
-    mv -f "$DOTTEMP"/tmp/skel "$DOTTEMP"/root/skel >/dev/null 2>&1
+    export GPG_TTY DOTTEMP
+    sudo rsync -ahq "$DOTTEMP"/root/skel/* /root/ &>/dev/null
+    sudo rsync -ahq "$DOTTEMP"/root/skel/* /etc/ &>/dev/null
+    sudo gpg --import "$DOTTEMP"/tmp/*.gpg 2>/dev/null
+    sudo gpg --import "$DOTTEMP"/tmp/*.sec 2>/dev/null
+    sudo gpg --import-ownertrust "$DOTTEMP"/tmp/*.trust 2>/dev/null
   fi
 
-  # Import gpg keys
+  # Import GPG keys
+  export GPG_TTY
   gpg --import "$DOTTEMP"/tmp/*.gpg 2>/dev/null
   gpg --import "$DOTTEMP"/tmp/*.sec 2>/dev/null
   gpg --import-ownertrust "$DOTTEMP"/tmp/*.trust 2>/dev/null
@@ -175,55 +177,53 @@ _files_init() {
   # import podcast feeds
   if cmd_exists castero; then
     if [[ -f "$HOME"/.config/castero/podcasts.opml ]]; then
-      castero --import "$HOME"/.config/castero/podcasts.opml >/dev/null 2>&1
+      castero --import "$HOME"/.config/castero/podcasts.opml &>/dev/null
     elif [[ -f "$DOTTEMP"/tmp/podcasts.opml ]]; then
-      castero --import "$DOTTEMP"/tmp/podcasts.opml >/dev/null 2>&1
+      castero --import "$DOTTEMP"/tmp/podcasts.opml &>/dev/null
     fi
   fi
 
   # import rss feeds
   if cmd_exists newsboat; then
     if [[ -f "$HOME"/.config/newsboat/news.opml ]]; then
-      newsboat -i "$HOME"/.config/newsboat/news.opml >/dev/null 2>&1
+      newsboat -i "$HOME"/.config/newsboat/news.opml &>/dev/null
     elif [[ -f "$DOTTEMP"/tmp/news.opml ]]; then
-      newsboat -i "$DOTTEMP"/tmp/news.opml >/dev/null 2>&1
+      newsboat -i "$DOTTEMP"/tmp/news.opml &>/dev/null
     fi
   fi
 
-  find "$HOME"/.gnupg "$HOME"/.ssh -type f -exec chmod 600 {} \; >/dev/null 2>&1
-  find "$HOME"/.gnupg "$HOME"/.ssh -type d -exec chmod 700 {} \; >/dev/null 2>&1
-  chmod 755 -f "$HOME" >/dev/null 2>&1
-  rm -Rf "$HOME/.local/share/mail/*/.keep" >/dev/null 2>&1
+  # finallize
+  find "$HOME"/.gnupg "$HOME"/.ssh -type f -exec chmod 600 {} \; &>/dev/null
+  find "$HOME"/.gnupg "$HOME"/.ssh -type d -exec chmod 700 {} \; &>/dev/null
+  chmod 755 -f "$HOME" &>/dev/null
+  rm -Rf "$HOME/.local/share/mail/*/.keep" &>/dev/null
   rm -Rf "$TMP_FILE"
-  mkdir -p "$HOME"/{Projects,Music,Videos,Downloads,Pictures,Documents}
-
+  mkdir -p "$HOME"/{Projects,Music,Videos,Downloads,Pictures,Documents} &>/dev/null
 }
 
 main() {
   if [ "$DOTTEMP" != "$DOTFILES" ]; then
-    if [ -d "$DOTTEMP" ]; then rm -Rf "$DOTTEMP" >/dev/null 2>&1; fi
+    if [ -d "$DOTTEMP" ]; then rm -Rf "$DOTTEMP" &>/dev/null; fi
   fi
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  printf_blue "\t\tSetting up the git repo: $GITREPO\n"
+  printf_blue "Setting up the git repo: $GIT_REPO"
   execute "_pre_inst" "Setting up"
   execute "_git_repo_init" "Initializing git repo"
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  printf_blue "\t\tThe installer is updating the scripts\n"
+  printf_blue "The installer is updating the scripts"
   execute "_scripts_init" "Installing scripts"
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  printf_blue "\t\tInstalling your personal files\n"
+  printf_blue "Installing your personal files"
   execute "_files_init" "Installing files"
   unset __colors DOTTEMP MIN UPDATE DESKTOP
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  printf_green "\t\tInstalling your personal files completed\n\n"
+  printf_green "Installing your personal files completed"
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # finally run main function
 main "$@"
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# End application
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# lets exit with code
-exit ${exitCode:-$?}
+exit "$?"
+# end

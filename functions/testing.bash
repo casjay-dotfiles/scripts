67088edd92ea082e0b63202d1dd0be620c22d831
Jason <git-admin@casjaysdev.com>
2021-11-25 21:23:21 -0500

    ❓ Added: desktopmgr  ❓

--- functions/testing.bash
+++ functions/testing.bash
@@ -95,6 +95,7 @@ DOTFILESREPO="https://github.com/dfmgr"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set other Repos
 DFMGRREPO="https://github.com/dfmgr"
+DESKTOPGRREPO="https://github.com/desktopmgr"
 PKMGRREPO="https://github.com/pkmgr"
 DEVENVMGRREPO="https://github.com/devenvmgr"
 DOCKERMGRREPO="https://github.com/dockermgr"
@@ -2174,6 +2175,30 @@ dfmgr_install() {
   __mkd "$USRUPDATEDIR"
   user_is_root && __mkd "$SYSUPDATEDIR"
   installtype="dfmgr_install"
+  #__main_installer_info
+}
+
+###################### desktopmgr settings ######################
+desktopmgr_install() {
+  user_installdirs
+  SCRIPTS_PREFIX="desktopmgr"
+  APPDIR="${APPDIR:-$CONF}"
+  INSTDIR="${INSTDIR:-$SHARE/CasjaysDev/$SCRIPTS_PREFIX}"
+  REPO="${REPO:-$DESKTOPMGRREPO}"
+  REPORAW="${REPORAW:-$DESKTOPMGRREPO/$APPNAME/raw/$GIT_REPO_BRANCH}"
+  USRUPDATEDIR="$SHARE/CasjaysDev/apps/$SCRIPTS_PREFIX"
+  SYSUPDATEDIR="$SYSSHARE/CasjaysDev/apps/$SCRIPTS_PREFIX"
+  ARRAY="$(grep -s '^' $CASJAYSDEVDIR/helpers/$SCRIPTS_PREFIX/array)"
+  LIST="$(grep -s '^' $CASJAYSDEVDIR/helpers/$SCRIPTS_PREFIX/list)"
+  #[ "$APPNAME" = "$SCRIPTS_PREFIX" ] && APPDIR="${APPDIR//$APPNAME\/$SCRIPTS_PREFIX/$APPNAME}"
+  if [ -f "$CASJAYSDEVSAPPDIR/dotfiles/$SCRIPTS_PREFIX-$APPNAME" ]; then
+    APPVERSION="$(grep -sv '#' $CASJAYSDEVSAPPDIR/dotfiles/$SCRIPTS_PREFIX-$APPNAME)"
+  else
+    APPVERSION="$currentVersion"
+  fi
+  __mkd "$USRUPDATEDIR"
+  user_is_root && __mkd "$SYSUPDATEDIR"
+  installtype="desktopmgr_install"
   #__main_installer_info
 }
 

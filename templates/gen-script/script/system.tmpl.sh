153ede0d94b81d589f67906b138fdb8525b3a9c2
Jason <git-admin@casjaysdev.com>
2022-07-11 00:57:54 -0400

    🗃️  modified: templates/gen-script/script/system.tmpl.sh 🗃️

--- templates/gen-script/script/system.tmpl.sh
+++ templates/gen-script/script/system.tmpl.sh
@@ -14,15 +14,15 @@
 # @Resource          :  ${res:-$get_res}
 # @sudo/root         :  no
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="\$(basename "\$0")"
-VERSION="\$GEN_SCRIPT_VERSION"
-HOME="\${USER_HOME:-\$HOME}"
-USER="\${SUDO_USER:-\$USER}"
-RUN_USER="\${SUDO_USER:-\$USER}"
-SRC_DIR="\${BASH_SOURCE%/*}"
+APPNAME="$(basename "$0")"
+VERSION="$GEN_SCRIPT_VERSION"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
+SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
-if [[ "\$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
-trap 'exitCode=\${exitCode:-\$?};[ -n "\$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && [ -f "\$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && rm -Rf "\$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" &>/dev/null' EXIT
+if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
+trap 'exitCode=${exitCode:-$?};[ -n "$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && [ -f "$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && rm -Rf "$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" &>/dev/null' EXIT
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

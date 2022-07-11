7d246da03fcb98bbcf4d3c2d499115d434d8f27b
Jason <git-admin@casjaysdev.com>
2022-07-11 10:56:50 -0400

    🗃️  modified: templates/gen-script/header/replace.tmpl.sh 🗃️

--- templates/gen-script/header/replace.tmpl.sh
+++ templates/gen-script/header/replace.tmpl.sh
@@ -1,17 +1,3 @@
 cat <<EOF | tee
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  $GEN_SCRIPT_VERSION
-# @Author            :  $GEN_SCRIPT_AUTHOR
-# @Contact           :  $GEN_SCRIPT_EMAIL
-# @License           :  $GEN_SCRIPT_DEFLICENSE
-# @ReadME            :  $GEN_SCRIPT_DEFREADME
-# @Copyright         :  $GEN_SCRIPT_COPYRIGHT
-# @Created           :  $GEN_SCRIPT_CREATED
-# @File              :  $GEN_SCRIPT_NEWFILE
-# @Description       :  ${desc:-}
-# @TODO              :  ${todo:-}
-# @Other             :  ${other:-}
-# @Resource          :  ${res:-}
-# @sudo/root         :  no
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+$(. "$CASJAYSDEVDIR/templates/gen-script/header/default.tmpl.sh")
 EOF

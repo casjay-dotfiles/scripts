52c0de1cdc4cad24f782f539527a53b0a16658c6
Jason <git-admin@casjaysdev.com>
2022-07-11 02:09:47 -0400

    🗃️  modified: templates/gen-script/readme/import_system.tmpl.sh 🗃️

--- templates/gen-script/readme/import_system.tmpl.sh
+++ templates/gen-script/readme/import_system.tmpl.sh
@@ -1,23 +1,10 @@
-#!/usr/bin/env bash
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME_README="$GEN_SCRIPT_REPLACE_APPNAME"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  $GEN_SCRIPT_VERSION
-# @Author            :  $GEN_SCRIPT_AUTHOR
-# @Contact           :  $GEN_SCRIPT_EMAIL
-# @License           :  $GEN_SCRIPT_DEFLICENSE
-# @ReadME            :  $(__filename $GEN_SCRIPT_NEWFILE 2>/dev/null) --help
-# @Copyright         :  $GEN_SCRIPT_COPYRIGHT
-# @Created           :  $GEN_SCRIPT_CREATED
-# @File              :  $(__filename $GEN_SCRIPT_NEWFILE 2>/dev/null)
-# @Description       :  ${desc:-$get_desc}
-# @TODO              :  ${todo:-$get_todo}
-# @Other             :  ${other:-$get_other}
-# @Resource          :  ${res:-$get_res}
-# @sudo/root         :  no
+cat <<EOF | tee
+$(. "$CASJAYSDEVDIR/templates/gen-script/header/default.tmpl.sh")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set variables
 __heading="- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME_README="$GEN_SCRIPT_REPLACE_APPNAME"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set functions
@@ -59,3 +46,4 @@ printf 'n'
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # lets exit with code
 exit "${exitCode:-$?}"
+EOF

e790ef30b407cf284417b5300b97d38490832a26
Jason <git-admin@casjaysdev.com>
2022-07-11 02:09:48 -0400

    🗃️  modified: templates/gen-script/script/system.tmpl.sh 🗃️

--- templates/gen-script/script/system.tmpl.sh
+++ templates/gen-script/script/system.tmpl.sh
@@ -1,28 +1,16 @@
-#!/usr/bin/env bash
+cat <<EOF | tee
+$(. "$CASJAYSDEVDIR/templates/gen-script/header/default.tmpl.sh")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  $GEN_SCRIPT_VERSION
-# @Author            :  $GEN_SCRIPT_AUTHOR
-# @Contact           :  $GEN_SCRIPT_EMAIL
-# @License           :  $GEN_SCRIPT_DEFLICENSE
-# @ReadME            :  $GEN_SCRIPT_DEFREADME
-# @Copyright         :  $GEN_SCRIPT_COPYRIGHT
-# @Created           :  $GEN_SCRIPT_CREATED
-# @File              :  $(__filename $GEN_SCRIPT_NEWFILE 2>/dev/null)
-# @Description       :  ${desc:-$get_desc}
-# @TODO              :  ${todo:-$get_todo}
-# @Other             :  ${other:-$get_other}
-# @Resource          :  ${res:-$get_res}
-# @sudo/root         :  no
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="$(basename "$0")"
-VERSION="$GEN_SCRIPT_VERSION"
-HOME="${USER_HOME:-$HOME}"
-USER="${SUDO_USER:-$USER}"
-RUN_USER="${SUDO_USER:-$USER}"
-SRC_DIR="${BASH_SOURCE%/*}"
+APPNAME="\$(basename "\$0")"
+VERSION="\$GEN_SCRIPT_VERSION"
+HOME="\${USER_HOME:-\$HOME}"
+USER="\${SUDO_USER:-\$USER}"
+RUN_USER="\${SUDO_USER:-\$USER}"
+SRC_DIR="\${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
-if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
-trap 'exitCode=${exitCode:-$?};[ -n "$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && [ -f "$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && rm -Rf "$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" &>/dev/null' EXIT
+if [[ "\$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
+trap 'exitCode=$\{exitCode:-\$?};[ -n "\$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && [ -f "\$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" ] && rm -Rf "\$GEN_SCRIPT_REPLACE_ENV_TEMP_FILE" &>/dev/null' EXIT
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+EOF

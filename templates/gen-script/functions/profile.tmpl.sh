b31fcc63bb3fdf94815949ed2ba9819bc5deaa4f
Jason <git-admin@casjaysdev.com>
2022-07-11 02:09:42 -0400

    🗃️  modified: templates/gen-script/functions/profile.tmpl.sh 🗃️

--- templates/gen-script/functions/profile.tmpl.sh
+++ templates/gen-script/functions/profile.tmpl.sh
@@ -1,12 +1,14 @@
+cat <<EOF | tee
 # Main function file
-if [ -f "$SRC_DIR/functions.bash" ]; then
-  FUNCTIONS_DIR="$SRC_DIR"
-  . "$FUNCTIONS_DIR/functions.bash"
-elif [ -f "$HOME/.local/bin/functions.bash" ]; then
-  FUNCTIONS_DIR="$HOME/.local/bin"
-  . "$FUNCTIONS_DIR/functions.bash"
+if [ -f "\$SRC_DIR/functions.bash" ]; then
+  FUNCTIONS_DIR="\$SRC_DIR"
+  . "\$FUNCTIONS_DIR/functions.bash"
+elif [ -f "\$HOME/.local/bin/functions.bash" ]; then
+  FUNCTIONS_DIR="\$HOME/.local/bin"
+  . "\$FUNCTIONS_DIR/functions.bash"
 else
-  printf "tt033[0;31m%s 033[0mn" "Could not source the functions file from $FUNCTIONS_DIR"
+  printf "tt033[0;31m%s 033[0mn" "Could not source the functions file from \$FUNCTIONS_DIR"
   return 1
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+EOF

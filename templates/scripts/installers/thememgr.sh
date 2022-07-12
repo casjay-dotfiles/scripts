66845f8c741de868a7c0c9643c5a5e519acb13c0
casjay <git-admin@casjaysdev.com>
2022-07-12 18:36:16 -0400

    🗃️  modified: templates/scripts/installers/thememgr.sh 🗃️

--- templates/scripts/installers/thememgr.sh
+++ templates/scripts/installers/thememgr.sh
@@ -15,8 +15,10 @@
 # @sudo/root         :  no
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 APPNAME="GEN_SCRIPT_REPLACE_APPNAME"
-USER="${SUDO_USER:-${USER}}"
-HOME="${USER_HOME:-${HOME}}"
+VERSION="GEN_SCRIPT_REPLACE_VERSION"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options

89b19475271748472918f109ef14505e67e1aaaf
casjay <git-admin@casjaysdev.com>
2022-07-13 18:06:05 -0400

    🗃️  modified: templates/gen-script/readme/new_system.tmpl.sh 🗃️

--- templates/gen-script/readme/new_system.tmpl.sh
+++ templates/gen-script/readme/new_system.tmpl.sh
@@ -4,28 +4,28 @@ $(. "$CASJAYSDEVDIR/templates/gen-script/header/default.tmpl.sh")
 # Set variables
 __heading="- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME_README="$GEN_SCRIPT_REPLACE_APPNAME"
+APPNAME_README="\$GEN_SCRIPT_REPLACE_APPNAME"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set functions
 __sed_head() { sed 's#..* :##g;s#^ ##g'; }
-__grep_head() { grep -sE '[".#]?@[A-Z]' "$(type -P "${2:-$GEN_SCRIPT_REPLACE_FILENAME}")" | grep "${1:-}"; }
-__version() { __grep_head 'Version' "$(type -P "$GEN_SCRIPT_REPLACE_FILENAME")" | __sed_head | head -n1 | grep '^'; }
-__printf_color() { printf "%b" "$(tput setaf "$2" 2>/dev/null)" "$1" "$(tput sgr0 2>/dev/null)"; }
-__printf_head() { __printf_color "ntt$__headingntt$2ntt$__headingn" "$1"; }
+__grep_head() { grep -sE '[".#]?@[A-Z]' "$(type -P "\${2:-\$GEN_SCRIPT_REPLACE_FILENAME}")" | grep "\${1:-}"; }
+__version() { __grep_head 'Version' "$(type -P "\$GEN_SCRIPT_REPLACE_FILENAME")" | __sed_head | head -n1 | grep '^'; }
+__printf_color() { printf "%b" "$(tput setaf "\$2" 2>/dev/null)" "\$1" "$(tput sgr0 2>/dev/null)"; }
+__printf_head() { __printf_color "\\n\\t\\t\$__heading\\n\\t\\t\$2\\n\\t\\t\$__heading\\n" "\$1"; }
 __printf_help() {
-  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="4"
-  local msg="$*"
+  test -n "\$1" && test -z "\${1//[0-9]/}" && local color="\$1" && shift 1 || local color="4"
+  local msg="\$*"
   shift
-  __printf_color "tt$msgn" "$color"
+  __printf_color "\\t\\t\$msgn" "\$color"
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-printf 'n'
+printf '\\n'
 __printf_head "5" "GEN_SCRIPT_REPLACE_FILENAME: GEN_SCRIPT_REPLACE_DESC"
 __printf_help " " "                                                  "
 __printf_help "5" "Usage: GEN_SCRIPT_REPLACE_APPNAME []"
 
-$([[ -f "$path_filename" ]] && for o in $(grep -s 'ARRAY=' $path_filename 2>/dev/null | grep -E ',| ' | head -n1 | sed 's|ARRAY=||g;s|,| |g;s|"||g' | tr ' ' 'n'); do echo '__printf_help "4" "'$(__filename $GEN_SCRIPT_NEWFILE 2>/dev/null)' '$o'  -  "'; done)
+$([[ -f "\$path_filename" ]] && for o in $(grep -s 'ARRAY=' \$path_filename 2>/dev/null | grep -E ',| ' | head -n1 | sed 's|ARRAY=||g;s|,| |g;s|"||g' | tr ' ' 'n'); do echo '__printf_help "4" "'$(__filename \$GEN_SCRIPT_NEWFILE 2>/dev/null)' '\$o'  -  "'; done)
 
 __printf_head "5" "Other GEN_SCRIPT_REPLACE_FILENAME Options"
 __printf_help "4" "GEN_SCRIPT_REPLACE_FILENAME --config              - Generate user config file"
@@ -37,10 +37,10 @@ __printf_help " " "                                                  "
 #__printf_help "4" "GEN_SCRIPT_REPLACE_FILENAME "
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end help
-printf 'n'
+printf '\\n'
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # End application
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # lets exit with code
-exit "${exitCode:-$?}"
+exit "\${exitCode:-\$?}"
 EOF

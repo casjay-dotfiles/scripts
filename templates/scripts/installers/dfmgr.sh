c17d520c06de347a6172e5be611ae631211491ef
casjay <git-admin@casjaysdev.com>
2022-07-12 23:06:12 -0400

    🗃️  modified: templates/scripts/installers/dfmgr.sh 🗃️

--- templates/scripts/installers/dfmgr.sh
+++ templates/scripts/installers/dfmgr.sh
@@ -150,14 +150,6 @@ fi
 # run post install scripts
 run_postinst() {
   dfmgr_run_post
-  if ! cmd_exits "$APPNAME" && [[ -f "$INSTDIR/build.sh" ]]; then
-    if builtin cd "$PLUGDIR/source"; then
-      BUILD_SRC_DIR="$PLUGDIR/source"
-      BUILD_SRC_URL=""
-      export BUILD_SRC_DIR BUILD_SRC_URL
-      eval "$INSTDIR/build.sh"
-    fi
-  fi
 }
 #
 execute "run_postinst" "Running post install scripts"
@@ -165,12 +157,22 @@ execute "run_postinst" "Running post install scripts"
 # create version file
 dfmgr_install_version
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# exit
+# run exit function
 run_exit
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run any external scripts
+if ! cmd_exists "$APPNAME" && [[ -f "$INSTDIR/build.sh" ]]; then
+  if builtin cd "$PLUGDIR/source"; then
+    BUILD_SRC_DIR="$PLUGDIR/source"
+    BUILD_SRC_URL="https://github.com/johanmalm/jgmenu"
+    export BUILD_SRC_DIR BUILD_SRC_URL
+    eval "$INSTDIR/build.sh"
+  fi
+fi
+cmd_exists $APPNAME || printf_red "jgmenu is not installed: run $INSTDIR/build.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lets exit with code
+exit ${EXIT:-$exitCode}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # End application
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# lets exit with code
-exit ${exitCode:-$?}
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# end

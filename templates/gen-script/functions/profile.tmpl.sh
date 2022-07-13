07d50e344cc9d65ed0c9a9aaf8862b7414b67ba2
casjay <git-admin@casjaysdev.com>
2022-07-13 18:05:55 -0400

    🗃️  modified: templates/gen-script/functions/profile.tmpl.sh 🗃️

--- templates/gen-script/functions/profile.tmpl.sh
+++ templates/gen-script/functions/profile.tmpl.sh
@@ -7,7 +7,7 @@ elif [ -f "\$HOME/.local/bin/functions.bash" ]; then
   FUNCTIONS_DIR="\$HOME/.local/bin"
   . "\$FUNCTIONS_DIR/functions.bash"
 else
-  printf "tt033[0;31m%s 033[0mn" "Could not source the functions file from \$FUNCTIONS_DIR"
+  printf "\\\t\\\t\\033[0;31m%s \\033[0m\\\n" "Couldn't source the functions file from \${FUNCTIONS_DIR:-\$SRC_DIR}"
   return 1
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

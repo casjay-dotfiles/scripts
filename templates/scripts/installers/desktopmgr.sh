b972486311935e3d01742aa29eeba3cbd9dc2d72
casjay <git-admin@casjaysdev.com>
2022-07-14 09:25:39 -0400

    ➕ Added: templates/scripts/installers/desktopmgr.sh ➕

---
+++ templates/scripts/installers/desktopmgr.sh
@@ -0,0 +1,187 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  GEN_SCRIPT_REPLACE_VERSION
+# @Author            :  GEN_SCRIPT_REPLACE_AUTHOR
+# @Contact           :  GEN_SCRIPT_REPLACE_EMAIL
+# @License           :  GEN_SCRIPT_REPLACE_LICENSE
+# @ReadME            :  GEN_SCRIPT_REPLACE_FILENAME --help
+# @Copyright         :  GEN_SCRIPT_REPLACE_COPYRIGHT
+# @Created           :  GEN_SCRIPT_REPLACE_DATE
+# @File              :  GEN_SCRIPT_REPLACE_FILENAME
+# @Description       :  GEN_SCRIPT_REPLACE_DESC
+# @TODO              :  GEN_SCRIPT_REPLACE_TODO
+# @Other             :  GEN_SCRIPT_REPLACE_OTHER
+# @Resource          :  GEN_SCRIPT_REPLACE_RES
+# @sudo/root         :  no
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="GEN_SCRIPT_REPLACE_APPNAME"
+VERSION="GEN_SCRIPT_REPLACE_VERSION"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
+SRC_DIR="${BASH_SOURCE%/*}"
+SCRIPTS_PREFIX="desktopmgr"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set bash options
+if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# specify any functions here
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import functions
+CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/desktopmgr/installer/raw/GEN_SCRIPT_REPLACE_DEFAULT_BRANCH/functions}"
+connect_test() { curl -q -ILSsf --retry 1 -m 1 "https://1.1.1.1" | grep -iq 'server:*.cloudflare' || return 1; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+elif connect_test; then
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/$SCRIPTSFUNCTFILE" || exit 1
+  . "/tmp/$SCRIPTSFUNCTFILE"
+else
+  echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
+  exit 90
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# trap the cleanup function
+trap_exit
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Call the main function
+desktopmgr_install
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# OS Support: supported_os unsupported_oses
+unsupported_oses
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Make sure the scripts repo is installed
+scripts_check
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Defaults
+APPNAME="${APPNAME:-GEN_SCRIPT_REPLACE_FILENAME}"
+APPDIR="$CONF/$APPNAME"
+INSTDIR="$CASJAYSDEVSHARE/desktopmgr/$APPNAME"
+REPO_BRANCH="${GIT_REPO_BRANCH:-main}"
+REPO="${DESKTOPMGR:-https://github.com/desktopmgr}/$APPNAME"
+REPORAW="$REPO/raw/$REPO_BRANCH"
+APPVERSION="$(__appversion "$REPORAW/version.txt")"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup plugins
+PLUGNAMES=""
+PLUGDIR="${SHARE:-$HOME/.local/share}/$APPNAME"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Require a version higher than
+desktopmgr_req_version "$APPVERSION"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Script options IE: --help --version
+show_optvars "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Do not update
+#installer_noupdate "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Requires root - no point in continuing
+#sudoreq "$0 *" # sudo required
+#sudorun  # sudo optional
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# initialize the installer
+desktopmgr_run_init
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run pre install commands
+run_pre_install
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end with a space
+APP="$APPNAME "
+PERL=""
+PYTH=""
+PIPS=""
+CPAN=""
+GEMS=""
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install packages - useful for package that have the same name on all oses
+install_packages "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install required packages using file
+install_required "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for perl modules and install using system package manager
+install_perl "$PERL"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for python modules and install using system package manager
+install_python "$PYTH"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for pip binaries and install using python package manager
+install_pip "$PIPS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for cpan binaries and install using perl package manager
+install_cpan "$CPAN"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for ruby binaries and install using ruby package manager
+install_gem "$GEMS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Other dependencies
+dotfilesreq
+dotfilesreqadmin
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Ensure directories exist
+ensure_dirs
+ensure_perms
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Backup if needed
+if [ -d "$APPDIR" ]; then
+  execute "backupapp $APPDIR $APPNAME" "Backing up $APPDIR"
+fi
+# Main progam
+if __am_i_online; then
+  if [ -d "$INSTDIR/.git" ]; then
+    execute "git_update $INSTDIR" "Updating $APPNAME configurations"
+  else
+    execute "git_clone $REPO $INSTDIR" "Installing $APPNAME configurations"
+  fi
+  # exit on fail
+  failexitcode $? "Git has failed"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Plugins
+if __am_i_online; then
+  if [ "$PLUGNAMES" != "" ]; then
+    if [ -d "$PLUGDIR/PLUREP/.git" ]; then
+      execute "git_update $PLUGDIR/PLUGREP" "Updating plugin PLUGNAME"
+    else
+      execute "git_clone PLUGINREPO $PLUGDIR/PLUGREP" "Installing plugin PLUGREP"
+    fi
+  fi
+  # exit on fail
+  failexitcode $? "Git has failed"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run post install scripts
+run_postinst() {
+  desktopmgr_run_post
+}
+#
+execute "run_postinst" "Running post install scripts"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# create version file
+desktopmgr_install_version
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run exit function
+run_exit
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# run any external scripts
+if ! cmd_exists "$APPNAME" && [[ -f "$INSTDIR/build.sh" ]]; then
+  if builtin cd "$PLUGDIR/source"; then
+    BUILD_SRC_DIR="$PLUGDIR/source"
+    BUILD_SRC_URL="https://github.com/johanmalm/jgmenu"
+    export BUILD_SRC_DIR BUILD_SRC_URL
+    eval "$INSTDIR/build.sh"
+  fi
+fi
+cmd_exists $APPNAME || printf_red "jgmenu is not installed: run $INSTDIR/build.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lets exit with code
+exit ${EXIT:-$exitCode}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End application
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

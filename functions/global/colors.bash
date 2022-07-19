0b99da03eaf55b032d18e18074e7603f1566817b
casjay <git-admin@casjaysdev.com>
2022-07-19 19:22:19 -0400

    🗃️ modified: functions/global/colors.bash 🗃️
    bin/am_i_online
    bin/anime-dl
    bin/anonymize
    bin/append_path
    bin/ask_for_input
    bin/ask_for_password
    bin/ask_yes_no_question
    bin/automate
    bin/backupapp
    functions/app-installer.bash
    functions/global/colors.bash
    functions/mgr-installers.bash
    functions/minimal.bash

--- functions/global/colors.bash
+++ functions/global/colors.bash
@@ -39,7 +39,7 @@ ICON_QUESTION="[ ❓ ]"
 __col() { awk -v col="$1" '{print $col}'; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 printf_newline() {
-  [[ -n "$1" ]] && printf '%s\n' "$*" || printf '\n'
+  [[ -n "$1" ]] && printf '%s' ${*:-} || printf '\n'
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if [ "$SHOW_RAW" = "true" ]; then

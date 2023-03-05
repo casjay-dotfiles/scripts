3c9825709a9b6cd64a7ce4f123833d42b657027d
casjay <git-admin@casjaysdev.com>
2023-03-05 14:01:37 -0500

    🗃️ Committing everything that changed 🗃️

--- templates/env/docker/other.sh
+++ templates/env/docker/other.sh
@@ -1,5 +1,5 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # other
-export INIT_DATE="\${INIT_DATE:-\$(date)}"
-export ENTRYPOINT_MESSAGE="\${ENTRYPOINT_MESSAGE:-no}"
+export INIT_DATE="${INIT_DATE:-$(date)}"
+export ENTRYPOINT_MESSAGE="${ENTRYPOINT_MESSAGE:-no}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- templates/env/docker/webservers.sh
+++ templates/env/docker/webservers.sh
@@ -1,6 +1,6 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # web server configs
-HTTPD_CONFIG_FILE="\${HTTPD_CONFIG_FILE:-\$(__find_httpd_conf)}"
-NGINX_CONFIG_FILE="\${NGINX_CONFIG_FILE:-\$(__find_nginx_conf)}"
-LIGHTTPD_CONFIG_FILE="\${LIGHTTPD_CONFIG_FILE:-\$(__find_lighttpd_conf)}"
+HTTPD_CONFIG_FILE="${HTTPD_CONFIG_FILE:-$(__find_httpd_conf)}"
+NGINX_CONFIG_FILE="${NGINX_CONFIG_FILE:-$(__find_nginx_conf)}"
+LIGHTTPD_CONFIG_FILE="${LIGHTTPD_CONFIG_FILE:-$(__find_lighttpd_conf)}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

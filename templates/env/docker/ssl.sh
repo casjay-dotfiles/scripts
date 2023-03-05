1953fd8bc1417f70f5c3b9dce6075b2b1a2f081d
casjay <git-admin@casjaysdev.com>
2023-03-05 16:11:14 -0500

    🗃️ Committing everything that changed 🗃️

--- templates/env/docker/ssl.sh
+++ templates/env/docker/ssl.sh
@@ -1,11 +1,11 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # ssl server settings
 SSL_ENABLED="${SSL_ENABLED:-false}"
-SSL_DIR="${SSL_DIR:-/config/ssl}"
+SSL_DIR="${SSL_CONTAINER_DIR:-/config/ssl}"
+SSL_DIR="${SSL_DIR:-$SSL_DIR}"
 SSL_CA="${SSL_CA:-$SSL_DIR/ca.crt}"
 SSL_KEY="${SSL_KEY:-$SSL_DIR/server.key}"
 SSL_CERT="${SSL_CERT:-$SSL_DIR/server.crt}"
-SSL_CONTAINER_DIR="${SSL_CONTAINER_DIR:-/etc/ssl/CA}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # certificate settings
 RSA="${RSA:-4096}"

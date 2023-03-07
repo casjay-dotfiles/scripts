2cf1fb69da3b2aedf9e4e44c997c0ea63b212361
casjay <git-admin@casjaysdev.com>
2023-03-07 17:01:49 -0500

    🗃️ Committing everything that changed 🗃️

--- templates/env/docker/other.sh
+++ templates/env/docker/other.sh
@@ -1,5 +1,4 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # other
-export INIT_DATE="${INIT_DATE:-$(date)}"
-export ENTRYPOINT_MESSAGE="${ENTRYPOINT_MESSAGE:-no}"
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

8cad7edefd4817acddb19a0a60984b2be7e137fd
casjay <git-admin@casjaysdev.com>
2023-03-08 22:25:17 -0500

    🗃️ Committing everything that changed 🗃️

--- templates/env/docker/global.sh
+++ templates/env/docker/global.sh
@@ -4,4 +4,5 @@ USER="${USER:-root}"
 LANG="${LANG:-C.UTF-8}"
 TZ="${TZ:-America/New_York}"
 SERVICE_USER="${SERVICE_USER:-root}"
+ENV_PORTS="${ENV_PORTS//\/*/}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

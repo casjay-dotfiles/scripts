711576a53abfba6b635ba5588fc4cd89ab5925d7
casjay <git-admin@casjaysdev.com>
2023-03-24 01:29:39 -0400

    🗃️  Committing everything that changed 🗃️

--- templates/env/docker/dockerd.sh
+++ templates/env/docker/dockerd.sh
@@ -1,6 +1,4 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# cerbot
-CERT_BOT_MAIL="${CERT_BOT_MAIL:-}"
-CERTBOT_DOMAINS="${CERTBOT_DOMAINS:-}"
-CERT_BOT_ENABLED="${CERT_BOT_ENABLED:-false}"
+# docker env
+DOCKER_HOST="${DOCKER_HOST:-unix://var/run/docker.sock}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

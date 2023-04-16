18186a93a1b9e60dd9089b155b926790d0c6d9ed
casjay <git-admin@casjaysdev.com>
2023-04-16 15:16:29 -0400

    🗃️  Committing everything that changed 🗃️

--- templates/scripts/completions/simple.bash
+++ templates/scripts/completions/simple.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
+# shellcheck disable=SC2317
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version           :  GEN_SCRIPT_REPLACE_VERSION
 # @@Author           :  GEN_SCRIPT_REPLACE_AUTHOR

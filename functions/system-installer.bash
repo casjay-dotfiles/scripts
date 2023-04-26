59668ab7ac46ad2f9dfd40453802844d1eaecb5f
casjay <git-admin@casjaysdev.com>
2023-04-26 13:16:08 -0400

    🗃️ Committing everything that changed 🗃️
    functions/app-installer.bash
    functions/mgr-installers.bash
    functions/system-installer.bash

--- functions/system-installer.bash
+++ functions/system-installer.bash
@@ -683,8 +683,22 @@ install_required() {
     if [ -f "$(builtin type -P pkmgr 2>/dev/null)" ]; then
       printf_yellow "Still missing: $MISSING"
       printf_yellow "Installing from package list"
-      pkmgr --enable-log dotfiles "$name" 2>/dev/null || { type -p $miss &>/dev/null && printf_green "$ICON_GOOD $name has been installed" || false; } || false
-      [ $? -eq 0 ] && REQUIRED="${REQUIRED//$miss/}" && __saved_file_create "$miss" || { printf_red "$ICON_ERROR $name failed to install" >&2 && false; }
+      if pkmgr --enable-log dotfiles "$name" 2>/dev/null; then
+        exitCode=0
+      elif type -p $miss | grep -q 'function'; then
+        printf_green "$ICON_GOOD $name is installed but is seems to be a function"
+        exitCode=0
+      else
+        exitCode=1
+        false
+      fi
+      if [ $exitCode -eq 0 ]; then
+        REQUIRED="${REQUIRED//$miss/}"
+        __saved_file_create "$miss"
+      else
+        printf_red "$ICON_ERROR $name failed to install" >&2
+        false
+      fi
     fi
     unset MISSING
     for cmd in $REQUIRED; do

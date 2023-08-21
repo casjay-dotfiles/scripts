ddb4bd290eadc56784795882f20821e4a504bd24
casjay <git-admin@casjaysdev.pro>
2023-08-20 20:17:24 -0400

    🗃️ Committing everything that changed 🗃️
    bin/detectostype
    functions/app-installer.bash
    functions/global/os.bash
    functions/mgr-installers.bash
    functions/minimal.bash
    functions/system-installer.bash

--- functions/global/os.bash
+++ functions/global/os.bash
@@ -27,9 +27,22 @@ __os_name() {
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 __os_version() {
-  grep -s '^VERSION=' /etc/os-release 2>/dev/null | sed 's|[a-zA-Z]||g;s/[^.0-9]*//g' | grep '^' ||
-    grep -s 'BUILD_ID' /etc/os-release 2>/dev/null | awk -F '=' '{print $2}' | sed 's|[a-zA-Z]||g;s/[^.0-9]*//g' | grep '^' ||
-    echo "Version: Unknown"
+  os_v=""
+  [ -n "$DISTRO_VERSION" ] && printf '%s\n' "$DISTRO_VERSION" | grep -q '^[0-9]' && printf '%s\n' "$DISTRO_VERSION" && return
+  if [ -z "$os_v" ]; then
+    os_v="$($LSB_RELEASE -a 2>/dev/null | grep -i '^Release' | grep -v '/' | awk '{print $2}' | grep '^' | grep '[0-9]')"
+  fi
+  if [ -z "$os_v" ]; then
+    os_v="$([ -f "/etc/debian_version" ] && cat "/etc/debian_version" | awk -F '.' '{print $1}' | grep '[0-9]$')"
+  fi
+  if [ -z "$os_v" ]; then
+    os_v="$(grep -s '^VERSION=' /etc/os-release 2>/dev/null | sed 's|[a-zA-Z]||g;s/[^.0-9]*//g' | grep -v '/' | grep '[0-9]$')"
+  fi
+  if [ -z "$os_v" ]; then
+    os_v="$(grep -s 'BUILD_ID' /etc/os-release 2>/dev/null | awk -F '=' '{print $2}' | sed 's|[a-zA-Z]||g;s/[^.0-9]*//g' | grep '[0-9]$')"
+  fi
+  [ -n "$os_v" ] && printf '%s\n' "$os_v" | sed 's|"||g' || echo "Version: Unknown"
+  unset os_v
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 __getpythonver() {

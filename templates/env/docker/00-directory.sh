272c04d81f22b23f0009c255a85a012f2f4c8de9
casjay <git-admin@casjaysdev.pro>
2023-08-21 16:08:36 -0400

    🗃️ Committing everything that changed 🗃️
    bin/dockermgr
    templates/env/docker/00-directory.sh

--- templates/env/docker/00-directory.sh
+++ templates/env/docker/00-directory.sh
@@ -1,7 +1,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # directory settings
+WWW_ROOT_DIR="${WWW_ROOT_DIR:-}"
 BACKUP_DIR="${BACKUP_DIR:-/data/backups}"
-WWW_ROOT_DIR="${WWW_ROOT_DIR:-/data/htdocs}"
 LOCAL_BIN_DIR="${LOCAL_BIN_DIR:-/usr/local/bin}"
 DATABASE_BASE_DIR="${DATABASE_BASE_DIR:-/data/db}"
 DEFAULT_DATA_DIR="${DEFAULT_DATA_DIR:-/usr/local/share/template-files/data}"

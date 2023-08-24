405f8c4593ca4af09ded1d5332bd978a6548d834
casjay <git-admin@casjaysdev.pro>
2023-08-24 17:37:25 -0400

    🗃️ Committing everything that changed 🗃️
    templates/env/docker/couchdb.sh
    templates/env/docker/global.sh
    templates/env/docker/mariadb.sh

--- templates/env/docker/couchdb.sh
+++ templates/env/docker/couchdb.sh
@@ -1,7 +1,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # couchdb env
-NODENAME="${NODENAME:-}"
-COUCHDB_USER="${DATABASE_USER_ROOT:-$COUCHDB_USER}"
-COUCHDB_PASSWORD="${DATABASE_PASS_ROOT:-$COUCHDB_PASSWORD}"
+COUCHDB_NODENAME="${COUCHDB_NODENAME:-$NODENAME}"
+COUCHDB_USER="${COUCHDB_USER:-$DATABASE_USER_ROOT}"
+COUCHDB_PASSWORD="${COUCHDB_PASSWORD:-$DATABASE_PASS_ROOT}"
 DATABASE_DIR_COUCHDB="${DATABASE_DIR_COUCHDB:-/data/db/couchdb}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- templates/env/docker/global.sh
+++ templates/env/docker/global.sh
@@ -3,6 +3,12 @@
 USER="${USER:-root}"
 LANG="${LANG:-C.UTF-8}"
 TZ="${TZ:-America/New_York}"
-SERVICE_USER="${SERVICE_USER:-root}"
 ENV_PORTS="${ENV_PORTS//\/*/}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+SERVICE_USER="${SERVICE_USER:-}"
+SERVICE_GROUP="${SERVICE_GROUP:-}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# How to set permissions
+CHANGE_USER="${SERVICE_USER:-}"
+CHANGE_GROUP="${SERVICE_GROUP:-}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

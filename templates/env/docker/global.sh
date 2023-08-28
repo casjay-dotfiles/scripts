72cd525b1eeb9f579c03a67520fb521344e1b305
casjay <git-admin@casjaysdev.pro>
2023-08-28 00:42:43 -0400

    🗃️ Committing everything that changed 🗃️
    templates/env/docker/global.sh
    templates/env/docker/networking.sh
    templates/scripts/functions/docker-entrypoint
    templates/scripts/other/start-service

--- templates/env/docker/global.sh
+++ templates/env/docker/global.sh
@@ -5,12 +5,9 @@ LANG="${LANG:-C.UTF-8}"
 TZ="${TZ:-America/New_York}"
 ENV_PORTS="${ENV_PORTS//\/*/}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# How to set permissions
 SERVICE_USER="${SERVICE_USER:-}"
 SERVICE_GROUP="${SERVICE_GROUP:-}"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# How to set permissions
-CHANGE_USER="${SERVICE_USER:-}"
-CHANGE_GROUP="${SERVICE_GROUP:-}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 SERVICE_UID="${SERVICE_UID:-}" # set the user id
 SERVICE_GID="${SERVICE_GID:-}" # set the group id

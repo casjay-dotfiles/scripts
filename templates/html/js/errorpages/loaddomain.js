3d183b98def6607f733dfd0cdf51a4de88b7ea05
casjay <git-admin@casjaysdev.pro>
2023-09-11 22:10:34 -0400

    🗃️ Committing everything that changed 🗃️
    templates/html/css/bootstrap.min.css
    templates/html/healthcheck/index.json
    templates/html/healthcheck/index.txt
    templates/html/index.html
    templates/html/js/errorpages/loaddomain.js
    templates/html/template.html

--- templates/html/js/errorpages/loaddomain.js
+++ templates/html/js/errorpages/loaddomain.js
@@ -1,7 +1,7 @@
 function loadDomain() {
-  let proto = location.protocol;
   let port = location.port;
   let url = location.hostname;
+  let proto = location.protocol;
   var display = document.getElementById('display-domain');
   display.innerHTML = proto + '//' + url + ':' + port;
 }

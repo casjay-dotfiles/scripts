f33481d91bcac058288e6c53744147424a031bea
casjay <git-admin@casjaysdev.pro>
2024-07-17 19:46:04 -0400

    🗃️ Committing everything that changed 🗃️
    templates/html/js/errorpages/loaddomain.js

--- templates/html/js/errorpages/loaddomain.js
+++ templates/html/js/errorpages/loaddomain.js
@@ -1,7 +1,7 @@
 function loadDomain() {
+  let proto = location.protocol;
   let port = location.port;
   let url = location.hostname;
-  let proto = location.protocol;
   var display = document.getElementById('display-domain');
-  display.innerHTML = proto + '//' + url + ':' + port;
+  display.innerHTML = `${req.protocol}://${req.get('host')}/${req.originalUrl}`;
 }

c6daf9e5eb52246d1e31fd771df81afa3a4430b0
casjay <git-admin@casjaysdev.pro>
2024-08-11 22:04:42 -0400

    🗃️ Committing everything that changed 🗃️
    templates/scripts/installers/dockermgr.sh

--- templates/scripts/installers/dockermgr.sh
+++ templates/scripts/installers/dockermgr.sh
@@ -1801,10 +1801,8 @@ if [ -n "$CONTAINER_OPT_PORT_VAR" ] || [ -n "$CONTAINER_ADD_CUSTOM_PORT" ]; then
         new_port="${new_port//random:/}"
         port="$random_port:${new_port//*:/}"
       elif echo "$new_port" | grep -q '\.all:[0-9]'; then
-        port="${new_port//.all/}"
-        if echo "$new_port" | grep -q ':.*[0-9]:[0-9]'; then
-          port="$port"
-        else
+        port="${new_port//.all:/}"
+        if ! echo "$new_port" | grep -q '.*[0-9]:[0-9]'; then
           port="$port:$port"
         fi
         set_listen_addr="false"

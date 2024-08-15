88d758303e25f6216b1b1f469585fdc3ab979308
casjay <git-admin@casjaysdev.pro>
2024-08-15 19:00:11 -0400

    🗃️ Committing everything that changed 🗃️
    templates/dockerfiles/init_scripts/03-files.sh

--- templates/dockerfiles/init_scripts/03-files.sh
+++ templates/dockerfiles/init_scripts/03-files.sh
@@ -30,17 +30,20 @@ exitCode=0
 # Predifined actions
 if [ -d "/tmp/bin" ]; then
   echo "Installing /tmp/bin to /usr/local/bin"
-  chmod -Rf +x "/tmp/bin"
+  chmod -Rf +x "/tmp/bin"/*
   copy "/tmp/bin/." "/usr/local/bin/"
 fi
 if [ -d "/tmp/etc" ]; then
   echo "Installing /tmp/etc to /etc"
-  copy "/tmp/etc/." "/etc/"
-  copy "/tmp/etc/." "/usr/local/share/template-files/config/"
+  copy "/tmp/etc"/* "/etc/"
+  echo "Installing /tmp/etc to /usr/local/share/template-files/config"
+  mkdir -p "/usr/local/share/template-files/config"
+  copy "/tmp/etc"/* "/usr/local/share/template-files/config/"
 fi
-if [ -d "/tmp/etc" ]; then
-  echo "Installing /tmp/etc to /etc"
-  copy "/tmp/data/." "/usr/local/share/template-files/data/"
+if [ -d "/tmp/data" ]; then
+  echo "Installing /tmp/data to /usr/local/share/template-files/data"
+  mkdir -p "/usr/local/share/template-files/data"
+  copy "/tmp/data"/* "/usr/local/share/template-files/data/"
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Main script

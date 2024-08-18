7abcc9396b19ff07e33547cd55e28dcd5b93f9bb
casjay <git-admin@casjaysdev.pro>
2024-08-18 09:35:02 -0400

    🗃️ Committing everything that changed 🗃️
    templates/html/js/errorpages/loaddomain.js

--- templates/html/js/errorpages/loaddomain.js
+++ templates/html/js/errorpages/loaddomain.js
@@ -2,10 +2,14 @@ function loadDomain() {
   let proto = location.protocol;
   let port = location.port || 80;
   let url = location.hostname;
-  let base = url.pathname.split('/')[1];
+  let base = location.pathname.split('/')[1];
   if (proto == 'https:') {
     port = location.port || 443;
   }
-  var display = document.getElementById('display-domain');
-  display.innerHTML = proto + '//' + url + ':' + port + base;
+  full_url = proto + '//' + url + ':' + port + '/' + base;
+  display = document.getElementById('display-domain');
+  display.href = full_url;
+  display.title = full_url;
+  display.innerHTML = full_url;
+  return full_url;
 }

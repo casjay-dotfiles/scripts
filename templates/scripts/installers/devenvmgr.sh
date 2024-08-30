52d6e93f53eeb937dcec7f42235bc429cec9a203
casjay <git-admin@casjaysdev.pro>
2024-08-29 21:49:36 -0400

    🗃️ Committing everything that changed 🗃️
    templates/scripts/installers/desktopmgr.sh
    templates/scripts/installers/devenvmgr.sh
    templates/scripts/installers/dfmgr.sh
    templates/scripts/installers/hakmgr.sh
    templates/scripts/installers/systemmgr.sh

--- templates/scripts/installers/devenvmgr.sh
+++ templates/scripts/installers/devenvmgr.sh
@@ -127,6 +127,8 @@ __system_service_start() { __system_service_active "$1" && systemctl restart "$1
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 __get_user_name() { grep ':' /etc/passwd | awk -F ':' '{print $1}' | sort -u | grep "^${1:-root}$" || return 1; }
 __get_user_group() { grep ':' /etc/group | awk -F ':' '{print $1}' | sort -u | grep "^${1:-root}$" || return 1; }
+__chuser() { local user=$1 && shift && grep -qs "^$user:" /etc/passwd && chown -Rf "$user" "$@" 2>/dev/null; }
+__chgroup() { local group=$1 && shift && grep -qs "$^group:" /etc/group && chgrp -Rf "$group" "$@" 2>/dev/null; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 sed="$(builtin type -P gsed 2>/dev/null || builtin type -P sed 2>/dev/null || return)"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

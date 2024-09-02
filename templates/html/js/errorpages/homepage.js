8cd54feb074b479ff7e1e91ea98351e6fd305770
casjay <git-admin@casjaysdev.pro>
2024-09-02 19:25:10 -0400

    🗃️ Committing everything that changed 🗃️
    templates/html/js/errorpages/homepage.js

--- templates/html/js/errorpages/homepage.js
+++ templates/html/js/errorpages/homepage.js
@@ -1,6 +1,16 @@
-function homepage() {
-  let proto = location.protocol;
+function homepage(getURI) {
+  let baseURI = getURI;
   let port = location.port;
-  let currentSite = window.location.hostname;
-  window.location = proto + '//' + currentSite + ':' + port;
+  let url = location.hostname;
+  let proto = location.protocol;
+  let baseURL = baseURI || location.pathname;
+  let base = baseURL.slice(0, baseURL.lastIndexOf('/'));
+  if (!base) base = '/';
+  if (!port)
+    if (proto == 'https:') {
+      port = 443;
+    } else {
+      port = 80;
+    }
+  window.location = proto + '//' + url + ':' + port + base;
 }

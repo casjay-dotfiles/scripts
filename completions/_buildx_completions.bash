dd1f11464f8471d77a4d45916f066795a258e5ea
casjay <git-admin@casjaysdev.pro>
2026-05-04 11:36:54 -0400

    🗃️ buildx: fix .build_failed and error log messaging 🗃️
    - .build_failed now records "Error log: <path>" (the .err file) instead of its own path
    - On failure: terminal shows "Error log: <err_path>" + "Failure summary: <.build_failed>"
    - __last_build_check: extracts "Error log:" from .build_failed and displays the actual .err path
    - Bump all versions to 202605041900-git
    
    bin/buildx
    completions/_buildx_completions.bash
    man/buildx.1

--- completions/_buildx_completions.bash
+++ completions/_buildx_completions.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202605041800-git
+##@Version           :  202605041900-git
 # @@Author           :  Jason Hempstead
 # @@Contact          :  jason@casjaysdev.pro
 # @@License          :  WTFPL

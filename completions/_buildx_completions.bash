3388638252e16bf688b504035a8482bb813d2467
casjay <git-admin@casjaysdev.pro>
2026-05-04 10:49:44 -0400

    ...🔧 buildx: fix org derivation ordering, registry compare, logging, dis...
    bin/buildx
    .claude/settings.local.json
    completions/_buildx_completions.bash
    man/buildx.1

--- completions/_buildx_completions.bash
+++ completions/_buildx_completions.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202605041300-git
+##@Version           :  202605041500-git
 # @@Author           :  Jason Hempstead
 # @@Contact          :  jason@casjaysdev.pro
 # @@License          :  WTFPL

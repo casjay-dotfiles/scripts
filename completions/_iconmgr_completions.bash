fe69e42719fb74f7f03aeee2ea68af24ed6e63d6
casjay <git-admin@casjaysdev.pro>
2026-06-04 09:27:36 -0400

    🐛 Fix iconmgr --help stray separator line and trailing blank 🐛
    - bin/iconmgr: " - " → ""
    remove trailing blank before closing divider
    - man/iconmgr.1: version stamp bumped
    - completions/_iconmgr_completions.bash: version stamp bumped
    
    bin/iconmgr
    completions/_iconmgr_completions.bash
    man/iconmgr.1

--- completions/_iconmgr_completions.bash
+++ completions/_iconmgr_completions.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202606040840-git
+##@Version           :  202606040927-git
 # @Author            :  Jason Hempstead
 # @Contact           :  jason@casjaysdev.pro
 # @License           :  WTFPL

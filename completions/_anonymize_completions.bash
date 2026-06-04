85a746bcfb48ea23618d94cfbed8cfe43c1cb0a3
casjay <git-admin@casjaysdev.pro>
2026-06-04 07:02:59 -0400

    📝 Sync man page, completions, and __help() for anonymize 📝
    All three were badly out of date (dated 2022, missing every command
    added since then). Full rewrite against current command set.
    man/anonymize.1:
    - Full rewrite: INSTALL, SERVICE, VPN, PROXY, STATUS, PRIVACY UTILITY,
    OPTION, EXAMPLE, CONFIGURATION, FILES, and DISPLAY DETECTION sections
    - Documents init, servers, tor, i2p, freenet, lokinet, zerotier,
    wireguard, proxy, vpn, status, stop, restart, check-ip, check-dns,
    harden, killswitch, mac, profile, monitor, speedtest, versions
    - Documents __has_display / __open_browser display-awareness behaviour
    - SEE ALSO: tor(1), i2prouter(1), lokinet(1), wg(8), openvpn(8), nmcli(1)
    - Date: 04 June 2026; version: 202606041900-git
    completions/_anonymize_completions.bash:
    - Removed ~40 lines of commented-out dead code
    - ARRAY and LONGOPTS updated to match current command set
    - Added servers subcommand with tor/i2p/freenet/lokinet choices
    - stop/restart extended to include lokinet
    - All case branches use consistent $COMP_CWORD variable
    - Version: 202606041900-git
    bin/anonymize __help():
    - Updated all descriptions to match current behaviour
    - Added servers subcommand entry; updated init description
    - Version bump to 202606041930-git
    
    bin/anonymize
    completions/_anonymize_completions.bash
    man/anonymize.1

--- completions/_anonymize_completions.bash
+++ completions/_anonymize_completions.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202602020740-git
+##@Version           :  202606041900-git
 # @@Author           :  Jason Hempstead
 # @@Contact          :  jason@casjaysdev.pro
 # @@License          :  WTFPL
@@ -8,29 +8,18 @@
 # @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
 # @@Created          :  Thursday, Jul 21, 2022 10:57 EDT
 # @@File             :  anonymize
-# @@Description      :  Anonymous web access
-# @@Changelog        :  Added completions for all new commands
-# @@TODO             :  Better documentation
-# @@Other            :
-# @@Resource         :
-# @@sudo/root        :  no
-# @@Template         :  completions/system
+# @@Description      :  Anonymous web access and privacy tool management
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 _anonymize_completion() {
-  ___findcmd() { find -L "${1:-$CONFDIR/}" -maxdepth ${3:-3} -type ${2:-f} 2>/dev/null | sed 's#'${1:-$CONFDIR}'##g' | grep '^' || return 1; }
   local CONFDIR="$HOME/.config/myscripts/anonymize"
-  local SEARCHDIR="${CONFDIR:-$HOME/.config/myscripts/anonymize}"
-  local CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
-  local CONFFILE="settings.conf"
   local cur prev words cword opts split
   local cur="${COMP_WORDS[$COMP_CWORD]}"
   local prev="${COMP_WORDS[$COMP_CWORD - 1]}"
-  #local SEARCHCMD="$(___findcmd "$SEARCHDIR/" "d" "1" | sort -u)"
   local SHOW_COMP_OPTS=""
   local FILEDIR=""
-  local LONGOPTS="--completions --config --debug --dir --help --options --no-color --version --silent --download --console --profile"
+  local LONGOPTS="--completions --config --console --debug --dir --download --help --no-color --options --silent --version"
   local SHORTOPTS=""
-  local ARRAY="init wireguard i2p freenet tor proxy zerotier status stop restart check-ip check-dns vpn lokinet harden killswitch mac profile monitor speedtest versions"
+  local ARRAY="init servers tor i2p freenet lokinet zerotier wireguard proxy vpn status stop restart check-ip check-dns harden killswitch mac profile monitor speedtest versions"
 
   _init_completion || return
 
@@ -41,156 +30,135 @@ _anonymize_completion() {
 
   if [[ ${cur} == --* ]]; then
     COMPREPLY=($(compgen -W '${LONGOPTS}' -- ${cur}))
+    return
   elif [[ ${cur} == -* ]]; then
     COMPREPLY=($(compgen -W '${SHORTOPTS:---}' -- ${cur})) && compopt -o nospace
-  else
-    case "${COMP_WORDS[1]:-$prev}" in
-    --debug | --no-color | --help | --version | --config | --options)
-      COMPREPLY=($(compgen -W '${ARRAY} ${LONGOPTS} ${SHORTOPTS}' -- ${cur}))
-      return 0
-      ;;
-    --freenet | --i2p | --download | --console)
-      COMPREPLY=($(compgen -W '' -- "$cur"))
-      ;;
-    init | i2p | freenet | tor | zerotier | lokinet)
-      COMPREPLY=($(compgen -W '' -- "$cur"))
-      ;;
-    status)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'tor i2p freenet zerotier vpn all' -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    stop)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'tor i2p freenet zerotier all' -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    restart)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'tor i2p freenet zerotier' -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    check-ip | check-dns | monitor | speedtest)
-      COMPREPLY=($(compgen -W '' -- "$cur"))
-      ;;
-    vpn)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'list connect disconnect status' -- "$cur"))
-      elif [[ $cword -eq 3 ]] && [[ "${COMP_WORDS[2]}" == "connect" ]]; then
-        # List available VPN connections
-        local vpns=$(nmcli connection show 2>/dev/null | grep -E 'vpn|wireguard' | awk '{print $1}')
-        COMPREPLY=($(compgen -W "$vpns" -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    harden)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'firefox chrome chromium' -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    killswitch)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'enable disable' -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    mac)
-      if [[ $cword -eq 2 ]]; then
-        # List network interfaces
-        local interfaces=$(ip link show 2>/dev/null | grep -E '^[0-9]+:' | cut -d: -f2 | tr -d ' ' | grep -v lo)
-        COMPREPLY=($(compgen -W "$interfaces" -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    profile)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'save load list delete' -- "$cur"))
-      elif [[ $cword -eq 3 ]]; then
-        local profiles=$(ls -1 "$HOME/.config/myscripts/anonymize/profiles" 2>/dev/null)
-        COMPREPLY=($(compgen -W "$profiles" -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    versions)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W 'check update' -- "$cur"))
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    wireguard)
-      [ $cword -eq 2 ] && _filedir || COMPREPLY=($(compgen -W '' -- "$cur"))
-      ;;
-    proxy)
-      if [[ $cword -eq 2 ]]; then
-        COMPREPLY=($(compgen -W '$(echo {0..9})' -- "$cur"))
-      elif [[ $cword -eq 3 ]]; then
-        COMPREPLY=($(compgen -c -- "${cur}"))
-      elif [[ $cword -eq 4 ]]; then
-        COMPREPLY=($(compgen -W 'https:// http://' -- "${cur}")) && compopt -o nospace
-      else
-        COMPREPLY=($(compgen -W '' -- "$cur"))
-      fi
-      ;;
-    *)
-      COMPREPLY=($(compgen -W '${ARRAY}' -- "$cur")) && compopt -o nospace
-      return 0
-      ;;
-    esac
+    return
   fi
-  #
-  # if [[ -n "$FILEDIR" ]]; then _filedir; fi
-  # if [[ $cword -gt 2 ]]; then
-  #   return
-  # elif [[ $cword == 2 ]]; then
-  #   _filedir
-  #   compopt -o nospace
-  #   return
-  # elif [[ $cword -eq 1 ]]; then
-  #   COMPREPLY=($(compgen -W '{a..z}{a..z}' -- "${cur}"))
-  #   compopt -o nospace
-  #   return
-  # if [[ "$ARRAY" = "show__none" ]]; then
-  #   COMPREPLY=($(compgen -W '' -- "${cur}"))
-  # elif [[ "$ARRAY" = "show__filedir" ]]; then
-  #   _filedir
-  # elif [[ "$ARRAY" = "show__commands" ]]; then
-  #   COMPREPLY=($(compgen -c -- "${cur}"))
-  # elif [ "$ARRAY" != "" ]; then
-  #   COMPREPLY=($(compgen -W '${ARRAY}' -- "${cur}"))
-  # elif [ -n "$OPTS" ]; then
-  #   COMPREPLY=($(compgen -W '${OPTS}' -- "${cur}"))
-  # else
-  #   COMPREPLY=($(compgen -W '${ARRAY}' -- "${cur}"))
-  # elif [[ $cword -gt 2 ]]; then
-  #   return
-  # elif [[ $cword == 2 ]]; then
-  #   _filedir
-  #   compopt -o nospace
-  #   return
-  # elif [[ $cword -eq 1 ]]; then
-  #   COMPREPLY=($(compgen -W '{a..z}{a..z}' -- "${cur}"))
-  #   compopt -o nospace
-  #   return
-  # fi
-  # [[ ${cword} == 2 ]] && _filedir && compopt -o nospace
-  # [[ $COMP_CWORD -eq 2 ] && COMPREPLY=($(compgen -W '{a..z} {A..Z} {0..9}' -o nospace -- "${cur}"))
-  # [[ $COMP_CWORD -eq 3 ] && COMPREPLY=($(compgen -W '$(_filedir)' -o filenames -o dirnames -- "${cur}"))
-  # [[ $COMP_CWORD -gt 3 ] && COMPREPLY=($(compgen -W '' -- "${cur}"))
-  # prev=""
-  # compopt -o nospace
+
+  case "${COMP_WORDS[1]:-$prev}" in
+  --debug | --no-color | --help | --version | --config | --options | --silent | --console | --download)
+    COMPREPLY=($(compgen -W '${ARRAY}' -- "${cur}"))
+    return 0
+    ;;
+
+  init | check-ip | check-dns | monitor | speedtest | zerotier)
+    COMPREPLY=($(compgen -W '' -- "${cur}"))
+    ;;
+
+  servers)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'tor i2p freenet lokinet' -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  tor | i2p | freenet | lokinet)
+    COMPREPLY=($(compgen -W '' -- "${cur}"))
+    ;;
+
+  status)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'tor i2p freenet zerotier vpn all' -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  stop)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'tor i2p freenet zerotier lokinet all' -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  restart)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'tor i2p freenet zerotier lokinet' -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  vpn)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'list connect disconnect status' -- "${cur}"))
+    elif [[ $COMP_CWORD -eq 3 ]] && [[ "${COMP_WORDS[2]}" == "connect" ]]; then
+      local vpns
+      vpns=$(nmcli connection show 2>/dev/null | grep -E 'vpn|wireguard' | awk '{print $1}')
+      COMPREPLY=($(compgen -W "$vpns" -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  wireguard)
+    [[ $COMP_CWORD -eq 2 ]] && _filedir || COMPREPLY=($(compgen -W '' -- "${cur}"))
+    ;;
+
+  proxy)
+    case $COMP_CWORD in
+    2) COMPREPLY=($(compgen -W '$(echo {1024..9999})' -- "${cur}")) ;;
+    3) COMPREPLY=($(compgen -c -- "${cur}")) ;;
+    4) COMPREPLY=($(compgen -W 'https:// http://' -- "${cur}")) && compopt -o nospace ;;
+    *) COMPREPLY=($(compgen -W '' -- "${cur}")) ;;
+    esac
+    ;;
+
+  harden)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'firefox chrome chromium' -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  killswitch)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'enable disable' -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  mac)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      local interfaces
+      interfaces=$(ip link show 2>/dev/null | grep -E '^[0-9]+:' | cut -d: -f2 | tr -d ' ' | grep -v lo)
+      COMPREPLY=($(compgen -W "$interfaces" -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  profile)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'save load list delete' -- "${cur}"))
+    elif [[ $COMP_CWORD -eq 3 ]]; then
+      local profiles
+      profiles=$(ls -1 "$CONFDIR/profiles" 2>/dev/null)
+      COMPREPLY=($(compgen -W "$profiles" -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  versions)
+    if [[ $COMP_CWORD -eq 2 ]]; then
+      COMPREPLY=($(compgen -W 'check update' -- "${cur}"))
+    else
+      COMPREPLY=($(compgen -W '' -- "${cur}"))
+    fi
+    ;;
+
+  *)
+    COMPREPLY=($(compgen -W '${ARRAY}' -- "${cur}"))
+    return 0
+    ;;
+  esac
+
   $split && return
 } &&
   # - - - - - - - - - - - - - - - - - - - - - - - - -

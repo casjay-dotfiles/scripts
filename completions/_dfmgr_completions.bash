2e1b06890ff0f964e3106e56a499ddf0748a1f1e
casjay <git-admin@casjaysdev.pro>
2026-06-04 09:22:34 -0400

    🐛 Fix dfmgr --help stray separator lines and trailing blank 🐛
    Replace two padded " - " spacer lines with clean empty lines and remove
    the spurious blank line before the Examples divider.
    - bin/dfmgr: 2x " - " → ""
    remove trailing blank before Examples divider
    - man/dfmgr.1: version stamp bumped
    - completions/_dfmgr_completions.bash: version stamp bumped
    
    bin/dfmgr
    completions/_dfmgr_completions.bash
    man/dfmgr.1

--- completions/_dfmgr_completions.bash
+++ completions/_dfmgr_completions.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202606040840-git
+##@Version           :  202606040922-git
 # @Author            :  Jason Hempstead
 # @Contact           :  jason@casjaysdev.pro
 # @License           :  WTFPL

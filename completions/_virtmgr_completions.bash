119201caa201faac2e19abd2b8e7737328dac58c
casjay <git-admin@casjaysdev.pro>
2026-07-18 20:34:20 -0400

    ✨ virtmgr delete: support multiple VM names in one call ✨
    - bin/virtmgr: delete case now loops over all given VM names,
    calling __check_vm_args and __vm_rm per name; usage error on
    zero args; exits non-zero if any deletion fails
    - bin/virtmgr: __help() delete line updated to show
    "delete <vm-name> [vm-name...]"
    - bin/virtmgr: version header and VERSION bumped to
    202607182200-git (also fixed pre-existing mismatch between
    the two); changelog updated
    - man/virtmgr.1: delete section updated to document multiple
    vm-name arguments; version stamp bumped to match
    - completions/_virtmgr_completions.bash: version stamp bumped
    to match (completion logic already supported multiple names
    since it dispatches on subcommand, not argument position)
    
    bin/virtmgr
    completions/_virtmgr_completions.bash
    man/virtmgr.1

--- completions/_virtmgr_completions.bash
+++ completions/_virtmgr_completions.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202606040840-git
+##@Version           :  202607182200-git
 # @Author            :  Jason Hempstead
 # @Contact           :  jason@casjaysdev.pro
 # @License           :  WTFPL
